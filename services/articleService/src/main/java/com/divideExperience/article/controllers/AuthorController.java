5eb69f4b57f9591eb206002b08243949322275d7
Anton Oleynikov <aoleynikov@fil-it.ru>
2019-03-03 13:13:05 +0300

    [DEXPER-5] Config and article services have been fixed.

--- services/articleService/src/main/java/com/divideExperience/article/controllers/AuthorController.java
+++ services/articleService/src/main/java/com/divideExperience/article/controllers/AuthorController.java
@@ -3,10 +3,7 @@ package com.divideExperience.article.controllers;
 import com.divideExperience.article.dataTransportObjects.AuthorItem;
 import com.divideExperience.article.facades.AuthorFacade;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by AOleynikov on 17.01.2019.
@@ -21,5 +18,17 @@ public class AuthorController {
     public void addAuthor(@RequestBody AuthorItem authorItem) {
         authorFacade.addAuthor(authorItem);
     }
+
+    @RequestMapping(value = "/get", method = RequestMethod.POST)
+    public AuthorItem getAuthor(@RequestParam Long authorId) {
+        return authorFacade.getAuthor(authorId);
+    }
+
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public void deleteAuthor(@RequestBody AuthorItem authorItem) {
+        authorFacade.deleteAuthor(authorItem);
+    }
+
 
 }
